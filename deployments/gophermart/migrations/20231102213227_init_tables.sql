4043706534a6a6612885c7c8abe75234db33c79c
Sergei Zaitcev <zzzaitcevsergei@yandex.ru>
2023-11-06 21:19:35 +0300

    Fix init tables

--- deployments/gophermart/migrations/20231102213227_init_tables.sql
+++ deployments/gophermart/migrations/20231102213227_init_tables.sql
@@ -2,45 +2,47 @@
 -- +goose StatementBegin
 create type order_status as enum ('new', 'processed', 'processing', 'invalid');
 
-create table if not exists users (id uuid default gen_random_uuid() not null primary key,
-user_name varchar(128) not null unique,
-user_password varchar(128) not null,
-created_at timestamp default now(),
-updated_at timestamp default now(),
-deleted_at timestamp);
+create table if not exists users (
+	id uuid default gen_random_uuid() not null primary key,
+	user_name varchar(128) not null unique,
+	hashed_password varchar(128) not null,
+	created_at timestamp default now(),
+	updated_at timestamp default now(),
+	deleted_at timestamp
+);
 
 create table if not exists orders (
-id uuid default gen_random_uuid() not null primary key,
-order_number int not null,
-status order_status,
-accrual int not null,
-created_at timestamp default now(),
-updated_at timestamp default now(),
-deleted_at timestamp,
-user_created uuid,
-foreign key (user_created) references users(id)
+	id uuid default gen_random_uuid() not null primary key,
+	order_number int not null,
+	status order_status,
+	accrual int not null,
+	created_at timestamp default now(),
+	updated_at timestamp default now(),
+	deleted_at timestamp,
+	user_created uuid,
+	foreign key (user_created) references users(id)
 );
 
 create table if not exists balance (
-id uuid default gen_random_uuid() not null primary key,
-user_id uuid not null,
-current_balance int not null,
-created_at timestamp default now(),
-updated_at timestamp default now(),
-deleted_at timestamp,
-foreign key (user_id) references users(id)
+	id uuid default gen_random_uuid() not null primary key,
+	user_id uuid not null,
+	current_balance int not null,
+	created_at timestamp default now(),
+	updated_at timestamp default now(),
+	deleted_at timestamp,
+	foreign key (user_id) references users(id)
 );
 
 create table if not exists balance_operations (
-id uuid default gen_random_uuid() not null primary key,
-order_id uuid,
-balance_id uuid,
-sum_operation int,
-created_at timestamp default now(),
-updated_at timestamp default now(),
-deleted_at timestamp,
-foreign key (order_id) references orders(id),
-foreign key (balance_id) references balance(id)
+	id uuid default gen_random_uuid() not null primary key,
+	order_id uuid,
+	balance_id uuid,
+	sum_operation int,
+	created_at timestamp default now(),
+	updated_at timestamp default now(),
+	deleted_at timestamp,
+	foreign key (order_id) references orders(id),
+	foreign key (balance_id) references balance(id)
 );
 -- +goose StatementEnd
 
@@ -52,7 +54,7 @@ drop table if exists balance;
 
 drop table if exists orders;
 
-drop table if exists users
+drop table if exists users;
 
-drop type order_status
+drop type order_status;
 -- +goose StatementEnd
