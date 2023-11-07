35eb3a56d9ab56cdbf334fe21ec982f09d366e09
Sergei Zaitcev <zzzaitcevsergei@yandex.ru>
2023-11-07 09:57:35 +0300

    Add luhn package

---
+++ pkg/luhn/luhn.go
@@ -0,0 +1,19 @@
+package luhn
+
+// Check проверяет контрольную сумму в последовательности цифр произвольной
+// длины по алгоритму Луны.
+func Check(value string) bool {
+	var sum int
+	parity := len(value) % 2
+	for i := 0; i < len(value); i++ {
+		digit := int(value[i] - '0')
+		if i%2 == parity {
+			digit *= 2
+			if digit > 9 {
+				digit -= 9
+			}
+		}
+		sum += digit
+	}
+	return sum%10 == 0
+}
